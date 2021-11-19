73a5ebca204e14fced1b65975bec8f292f866567
Peter Kaszt <kasztp@gmail.com>
2021-11-19 09:00:36 +0100

    Refactor: Separate business logic from routes.

---
+++ app/shortener_logic.py
@@ -0,0 +1,56 @@
+from app import db
+from app.models import URLStore, Logs
+
+
+def shortened_validator(data):
+    url_check = URLStore.query.filter_by(shortened=data).first()
+    if url_check:
+        print(f'URL Already in DB as: {url_check.shortened}')
+        return url_check.original_url
+    else:
+        print('URL Not yet in DB.')
+        return False
+
+
+def logger(ip, url):
+    """Basic logging to DB"""
+    log_entry = Logs(ip_address=ip, endpoint=url)
+    db.session.add(log_entry)
+    db.session.commit()
+
+
+def to_base_62(number):
+    """Convert decimal integer to base 62 for further shortening"""
+    b_62 = str()
+    digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    while number != 0:
+        number, i = divmod(number, len(digits))
+        b_62 = digits[i] + b_62
+    return b_62
+
+
+def shortener(data):
+    original_url = data["payload"]
+    url_check = URLStore.query.filter_by(original_url=original_url).first()
+    if url_check:
+        print(f'URL Already in DB as: {url_check.shortened}')
+        shortened = 'tier.app/' + url_check.shortened
+    else:
+        print('URL Not yet in DB.')
+        db_entry = URLStore(original_url=original_url)
+        db.session.add(db_entry)
+        db.session.commit()
+
+        url_temp = URLStore.query.filter_by(original_url=original_url).first()
+        shortened = str(to_base_62(url_temp.id))
+        url_temp.shortened = shortened
+        db.session.commit()
+
+        print(f'{url_temp.id} -> {shortened}')
+        shortened = 'tier.app/' + url_temp.shortened
+    response = {
+        "original": data["payload"],
+        "shortened": shortened,
+        "is_url": True,
+    }
+    return response
